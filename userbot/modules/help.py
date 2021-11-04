5a73fd97b206797781e57e72a46b2cd42a61bd1e
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-04 12:32:10 +0530

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -1,34 +1,34 @@
 from telethon import events
-from userbot import help
+from userbot import CMD_HELP
 import asyncio
 
 @events.register(events.NewMessage(pattern="!help"))
 async def help(event):
     args = event.pattern_match.group(1).lower()
     if args:
-        if args in help:
-            await event.edit(str(help[args]))
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
         else:
             await event.edit("`Unknown module type !help to see all modules`")
     else:
         await event.edit(" **Support Channel for help & report bugs @tilak_sharma_2004** ")
-        string = (f"`Use !help <module_name>`\n\n**Currently Loaded [{len(help)}] Modules **\n")
+        string = (f"`Use !help <module_name>`\n\n**Currently Loaded [{len(CMD_HELP)}] Modules **\n")
         for i in CMD_HELP:
             string += f" `{str(i)} `{str(i)}` `{str(i)}` "
         await event.reply(string)
         
         
-help.update({
+CMD_HELP.update({
     "alive":
     "do !jinda for alive your userbot"
 })
 
-help.update({
+CMD_HELP.update({
     "ping":
     "do !check to ping your userbot"
 })
 
-help.update({
+CMD_HELP.update({
     "pmpermit":
     "do !allow to allow someone to pm\ndo !disallow to disallow someone to pm\ndo !listallowed to get the list of allowed users in pm"
 })
