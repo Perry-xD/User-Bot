ac8b59f589a09227e3af2cf8030f8c55acec4854
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-04 14:14:48 +0530

    Update ping.py

--- userbot/modules/ping.py
+++ userbot/modules/ping.py
@@ -1,6 +1,6 @@
 from telethon import events
 import asyncio
 
-@events.register(events.NewMessage(pattern="!check"))
+@bot.on(events.NewMessage(pattern="!check"))
 async def eval_(e):
     await e.edit("**✔USERBOT ONLINE!**")
