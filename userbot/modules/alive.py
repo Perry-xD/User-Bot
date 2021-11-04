84dabd4d4cd4c781dea978d662bc06cfbcac8693
GodBoyX <87893142+Perry-xD@users.noreply.github.com>
2021-11-04 13:54:21 +0530

    Update alive.py

--- userbot/modules/alive.py
+++ userbot/modules/alive.py
@@ -1,7 +1,7 @@
-from telethon import events
+from telethon import events, client
 import asyncio
 
 @events.register(events.NewMessage(pattern="!jinda"))
 async def alive_me(event):
-  me = await bot.get_me()
+  me = await client.get_me()
   await event.edit(f"**Aha Meh Alive {me.first_name} Sir!**")
